72df260c2d0b62f87d03f3e71604595bfe25a05f
Neil Ellis <neil@cazcade.com>
2014-10-07 23:04:31 +0100

    The secret of life is honesty and fair dealing. If you can fake that, you've got it made.

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash -eu
 ./install_weave.sh
 echo $1 > ~/.weave_host_id
-weave launch "10.0.0.$((100 + ${1}))/24"
+echo "Launching Weave"
+(sleep 10 & weave launch "10.0.0.$((100 + ${1}))/24") &
+disown
