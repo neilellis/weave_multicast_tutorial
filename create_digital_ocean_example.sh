c65ad7470868f8b1e10fdc549447eeafc423252c
Neil Ellis <neil@cazcade.com>
2014-10-08 02:43:06 +0100

    With no power, comes no responsibility.

--- create_digital_ocean_example.sh
+++ create_digital_ocean_example.sh
@@ -51,9 +51,9 @@ do
     done
     sleep 20
     ips=$(tugboat droplets | grep ^weave-multicast-demo- | cut -d: -f2 | cut -d, -f1 | tr -d ' ')
-    tugboat ssh -c "apt-get -y -q install git; git clone https://github.com/cazcade/weave_multicast_tutorial.git; cd weave_multicast_tutorial; chmod +x *.sh; ./install.sh ${i}"  weave-multicast-demo-${i}
+    tugboat ssh -c "apt-get -y -q install git; git clone https://github.com/cazcade/weave_multicast_tutorial.git; cd weave_multicast_tutorial; chmod +x *.sh; ./install.sh ${i}  ${ips}"  weave-multicast-demo-${i}
     sleep 20
-    tugboat ssh -c " cd weave_multicast_tutorial; ./run_demo.sh ${i} ${ips}"  weave-multicast-demo-${i}
+    tugboat ssh -c " cd weave_multicast_tutorial; ./run_demo.sh ${i}"  weave-multicast-demo-${i}
 done
 
 echo
--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -eu
 ./install_weave.sh
-echo $1 > ~/.weave_host_id
+host_id=$1
+echo ${host_id} > ~/.weave_host_id
+shift
 echo "Launching Weave"
-echo  "sleep 1 ; weave launch \"10.0.0.$((100 + ${1}))/24\"" | at now
+echo  "sleep 1 ; weave launch \"10.0.0.$((100 + ${host_id}))/24\" $@" | at now
 
