5433a147332c38935c6f0d565254b3984fc5fedd
Neil Ellis <neil@cazcade.com>
2014-10-08 01:39:28 +0100

    Carry on my wayward sons ...

--- run.sh
+++ run.sh
@@ -8,10 +8,10 @@ do
 done
 ip=$(ip addr show ethwe | grep inet | grep -v inet6 | sed 's/^[ ]*inet //g'| cut -d/ -f 1)
 
-./mcreceive 239.1.2.3 1234
+/mcreceive 239.1.2.3 1234
 
 echo "Welcome to the Weave Multicast Chat Demo"
 while read -p $1 line
 do
-    echo "$1> $line" | ./mcsend 239.1.2.3 1234
+    echo "$1> $line" | /mcsend 239.1.2.3 1234
 done
--- run_demo.sh
+++ run_demo.sh
@@ -3,7 +3,8 @@ host_id=$(<~/.weave_host_id)
 network="10.0.0.${host_id}/24"
 echo "Network for this host is $network"
 weave run $network -t -i cazcade/weave-multicast-tutorial /run.sh ${host_id}
-docker ps | grep  a | head -1 | cut -d" " -f1 > ~/.container_id
+docker ps --no-trunc | grep  weave-multicast-tutorial | head -1 | cut -d" " -f1 > ~/.container_id
+docker attach $(cat ~/.container_id)
 
 #NSPID=$(docker inspect --format='{{ .State.Pid }}' $C)
 #ln -s /proc/$NSPID/ns/net /var/run/netns/$NSPID
