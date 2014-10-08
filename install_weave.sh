3838bb4b2e868d748cb72f5debf91491211924bf
Neil Ellis <neil@cazcade.com>
2014-10-08 03:48:36 +0100

    Chaos reigns within. Reflect, repent, and reboot. Order shall return.

--- install_weave.sh
+++ install_weave.sh
@@ -2,5 +2,5 @@
 sudo wget -q -O /usr/local/bin/weave \
   https://raw.githubusercontent.com/zettio/weave/master/weaver/weave
 sudo chmod a+x /usr/local/bin/weave
-apt-get -y -q install ethtool conntrack
+apt-get -y -qq install ethtool conntrack
 
