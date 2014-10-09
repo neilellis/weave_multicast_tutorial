28bfb6f323369d7795b25ffa3693743dcab63153
Neil Ellis <neil@cazcade.com>
2014-10-10 00:56:08 +0100

    Time flies like an arrow - Fruit flies like a banana

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -eu
 ./install_weave.sh
-ufw allow 6783
+#ufw allow 6783
 host_id="${1}"
 shift
 echo ${host_id} > ~/.weave_host_id
 echo "Launching Weave"
-echo  "sleep 2 ; weave launch \"10.0.0.$((100 + ${host_id}))/24\" $@" | at now
+(echo  "sleep 2 ; weave launch \"10.0.0.$((100 + ${host_id}))/24\" $@" | at now ) &> /dev/null
 
--- run_demo.sh
+++ run_demo.sh
@@ -4,11 +4,5 @@ network="10.0.0.${host_id}/24"
 echo "Network for this host is $network"
 weave run $network -t -i neilellis/weave-multicast-tutorial /run.sh ${host_id}
 docker ps --no-trunc | grep  weave-multicast-tutorial | cut -d" " -f1 > ~/.container_id
-#sudo docker-ns $(cat ~/.container_id) route add -net 224.0.0.0 netmask 240.0.0.0 dev ethwe   || :
 sleep 5
 docker logs $(cat ~/.container_id)
-
-#NSPID=$(docker inspect --format='{{ .State.Pid }}' $C)
-#ln -s /proc/$NSPID/ns/net /var/run/netns/$NSPID
-#ip netns exec $NSPID route add -net 224.0.0.0 netmask 240.0.0.0 dev ethwe
-
