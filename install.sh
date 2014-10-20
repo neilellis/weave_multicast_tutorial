4d914c72127f11a787fe25af30920d7349fbb72e
Neil Ellis <neil@cazcade.com>
2014-10-20 18:06:36 +0100

    Corrections

--- install.sh
+++ install.sh
@@ -5,5 +5,5 @@ host_id="${1}"
 shift
 echo ${host_id} > ~/.weave_host_id
 echo "Launching Weave"
-(echo  "sleep 2 ; weave launch \"10.0.0.$((100 + ${host_id}))/16\" $@" | at now ) &> /dev/null
+(echo  "sleep 2 ; weave launch \"10.0.0.$((100 + ${host_id}))/24\" $@" | at now ) &> /dev/null
 
